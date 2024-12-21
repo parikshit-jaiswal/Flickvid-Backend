4ad4e4092dd7b774ffe8ee0b99fa5d9de909a449
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2024-12-22 00:47:24 +0530

    custom API error annd responce handler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
-
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+const port = process.env.PORT || 8080;
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server started on port : ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB connection failed !!! ", err)
+    })
 
 
 // import express from 'express'
