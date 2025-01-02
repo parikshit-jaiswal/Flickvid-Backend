fbf18a5435e68642d83ad1e74848a52d6867f807
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2025-01-02 23:26:47 +0530

    Aurh middleware done

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import ApiError from "../utils/ApiError";
+import asyncHandler from "./asyncHandler";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized requiest");
+        }
+        const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodeToken?._id).select("-password -refreshToken");
+
+        if (!user) {
+            throw new ApiError(401, "Invalid token");
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid token");
+    }
+})
